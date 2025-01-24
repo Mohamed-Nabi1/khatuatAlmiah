d9c444568b4d65a87305cdf4bc4e487a9d32ff95
Amr434 <amrkasban37@gmail.com>
2025-01-24 17:00:42 +0200

    add footer v1.0

--- assets/scripts/components/footer.js
+++ assets/scripts/components/footer.js
@@ -1,5 +1,18 @@
-export function renderFooter(){
- const footer=document.getElementById('footer');
- footer.innerHTML=`footer`;
-
+import { loadCSS } from "../utils/cssloader.js";
+export function renderFooter() {
+  loadCSS("./assets/styles/footer.css");
+  const footer = document.getElementById("footer");
+  footer.innerHTML = ` <div class="container">
+            <div class="social-links mb-3">          
+                <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+                <a href="https://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="https://linkedin.com" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="privacy">
+            <a href="#">سياسة الخصوصية</a>
+            <a href="">الأسئلة الشائعة</a>
+            </div>
+            <p>.&copy;2023 جميع الحقوق محفوظة لشركة خطوات المياه</p>
+        </div>`;
 }
