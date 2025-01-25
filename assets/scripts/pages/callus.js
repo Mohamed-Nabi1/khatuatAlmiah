69563462ec096e7d7357526e757aa4513f73ea9c
Mohamed-Nabil <mohamedd.nabil3@gmail.com>
2025-01-25 18:44:58 +0200

    add call us

--- assets/scripts/pages/callus.js
+++ assets/scripts/pages/callus.js
@@ -6,21 +6,20 @@ export function renderCallUs(mainContent) {
   loadCSS("./assets/styles/callus.css");
 
   mainContent.innerHTML = `
-    <div class="full-width-bg text-center py-5 bg-primary text-white">
-        <div class="row justify-content-center">
-          <div class="col-md-4">
-            <img class="img-fluid mb-3" src="./assets/images/image.png" alt="khatuat altaqnia Systems Logo">
-          </div>
-          <div class="col-md-8">
-            <h2 class="mb-3">لا تتردد في التواصل معنا</h2>
-            <p class="mb-3">للحصول على أفضل حلول أنظمة المياه</p>
-            <p class="mb-3">
-              <strong>هاتف:</strong> <a class="text-white text-decoration-underline" href="tel:0000000">0000000</a><br>
-              <strong>بريد إلكتروني:</strong> <a class="text-white text-decoration-underline" href="mailto:000000">00000</a><br>
-              <strong>العنوان:</strong> السعودية
-            </p>
-          </div>
-        </div>
-      </div>
+    <div class="container text-center">
+      <!-- Logo Section -->
+      <img src="./assets/images/image.png" alt="logo image" class="img-fluid w-50 p-5">
+
+
+      <!-- Content Section -->
+      <div class="row content-section w-100 p-5">
+      <h2>تواصل معنا</h2>
+      <p>نسعد بخدمتك وتقديم أفضل حلول أنظمة المياه</p>
+      <p>
+        <strong>هاتف:</strong> <a href="tel:000000000">000000000</a><br>
+        <strong>بريد إلكتروني:</strong> <a href="mailto:example@example.com">example@example.com</a><br>
+        <strong>العنوان:</strong> المملكة العربية السعودية
+      </p>
+  </div>
   `;
 }
