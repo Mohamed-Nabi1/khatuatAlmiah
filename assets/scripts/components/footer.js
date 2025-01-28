43eca134879b21ca9f49ba3d8fd0bec0b051fb5d
Mohamed-Nabil <mohamedd.nabil3@gmail.com>
2025-01-28 19:23:27 +0200

    modify name

--- assets/scripts/components/footer.js
+++ assets/scripts/components/footer.js
@@ -7,7 +7,6 @@ export function renderFooter() {
                 <a href="https://facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
                 <a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
                 <a href="https://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
-                <a href="https://linkedin.com" target="_blank"><i class="fab fa-linkedin-in"></i></a>
             </div>
             <p>الرقم الضريبي : 312095937500003</p>
             <p>&copy;2025 جميع الحقوق محفوظة لشركة خطوات التنقيه التجاريه</p>
